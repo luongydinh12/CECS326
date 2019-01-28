3a837bd98d0f9db7ec725c1bf1dbcae6e61ebd9f
Dinh Luong <luongydinh12@gmail.com>
2019-01-27 18:04:16 -0800

    1/27 6PM

--- CECS326/Lab1/cecs326_Lab1.cpp
+++ CECS326/Lab1/cecs326_Lab1.cpp
@@ -1,11 +1,18 @@
+
+// Dinh Luong
+// cecs326_Lab1.cpp
+// Description:
+
+
 #include <iostream>
 
 using namespace std;
-struct lab1
+char alphabet[26] = { 'A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+struct myArray
 {   
-    int n = 2700;
-    char *ch =  new char[20]; 
-    int *in = new int [20];
+    char *ch[20]; 
+    int *in[20];
 };
 
 
@@ -24,9 +31,17 @@ void menu(){
 }
 void submenu(){
     cout << "Sub Menu: \n";
-    }
+}
 
 int recursive(int n) {
-    n = 2*recursive(n);
+    if (n == 0)
+        return 2700;
+    else
+        return 2*recursive(n-1);
     return n;
-}
+} //  Recursive Function: f(0)=2700 & f(n+1)=2*f(n), Generate indexes of integer array
+
+char randomCharacter(){
+    int randomIndex = rand()%26;
+    return alphabet[randomIndex] ;
+} // Function to generate and return a random character
